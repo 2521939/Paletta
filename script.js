2daf17b632d3af20e0fb4c048673dec408f0fd1f
NoL <2521466@unicesusc.edu.br>
2026-05-13 19:50:00 -0300

    Criado novo Branch (Joao), e complementado o CSS e Javascript do modal de Login

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
-const abrirmodal = document.getElementById('openBtn');
-const myModal = document.getElementById('myModal')
+const abrirmodal = document.getElementById('abrirmodal');
+const modalLogin = document.getElementById('modalLogin')
+const fecharmodalbtn = document.getElementById('fecharmodalbtn')
 
-abrirmodal.addEventListener('click', () => {
-    myModal.showModal(); 
-});
+/*teste de event listener, pode simplesmente fazer on click
+quando é um botão e usar a função
+*/
+
+abrirmodal.addEventListener('click', funcaomodalabrir);
+
+function funcaomodalabrir(){
+    modalLogin.showModal();
+};
+
+
+fecharmodalbtn.addEventListener('click', funcaomodalfechar);
+
+function funcaomodalfechar(){
+    modalLogin.close();
+};
