75d7d2ecebc0361a85058b9cd48488113b0d25f3
NoL <2521466@unicesusc.edu.br>
2026-05-14 17:27:58 -0300

    Adicionado Botões ao Carrosel e Funcionalidade via CSS e Javascript, Adicionado a aba Explorar

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
-const abrirmodal = document.getElementById('abrirmodal');
-const modalLogin = document.getElementById('modalLogin')
-const fecharmodalbtn = document.getElementById('fecharmodalbtn')
+let abrirmodal = document.getElementById('abrirmodal')
+let modalLogin = document.getElementById('modalLogin')
+let fecharmodalbtn = document.getElementById('fecharmodalbtn')
+let PalettaLogo = document.getElementById('IndexRedir')
+
+let janslides = document.getElementById('janelas-slide')
+let slides = document.querySelectorAll('.slide')
 
 /*teste de event listener, pode simplesmente fazer on click
 quando é um botão e usar a função
@@ -19,3 +23,32 @@ fecharmodalbtn.addEventListener('click', funcaomodalfechar);
 function funcaomodalfechar(){
     modalLogin.close();
 };
+
+// Referente aos Slides
+
+
+
+function moverdireita() {
+    let pos_slide1 = slides[0].offsetLeft
+    let pos_slide2 = slides[1].offsetLeft
+
+    let distanciafinal = pos_slide2 - pos_slide1
+        janslides.scrollBy({
+            left: distanciafinal,
+            behavior: 'smooth'
+        })
+    }
+
+function moveresquerda() {
+    let pos_slide1 = slides[0].offsetLeft
+    let pos_slide2 = slides[1].offsetLeft
+
+    let distanciafinal = pos_slide2 - pos_slide1
+        janslides.scrollBy({
+            left: -distanciafinal,
+            behavior: 'smooth'
+        })
+    }
+
+
+
