f1f8cc8e280dbfbd5f81e592a39896506d6875bf
João <2521466@unicesusc.edu.br>
2026-06-11 19:24:46 -0300

    Alterado variaveis incorretas, e iniciado testes com o Node

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-var url = "http://localhost:8080/users";
+var url = "http://localhost:3000/posts";
 const headers = {
     'Content-Type': 'application/json; charset=utf-8',  
     "X-User": "fulano",                                 
@@ -25,9 +25,9 @@ async function carregarDados() {
             let sectionscript = document.createElement("section"); // cria uma linha da tabela
             sectionscript.innerHTML = (`
 
-                <section class="post-thumb" style="cursor: pointer onclick="abrirpost(${row.id})";">
-                    <h3>${row.nome-post}</h3>
-                    <p>ID do Post: ${row.id}</p>
+                <section class="post-thumb" style="cursor: pointer onclick="abrirpost(${row.idPos})";">
+                    <h3>${row.nome}</h3>
+                    <p>ID do Post: ${row.idPos}</p>
                 </section>
             `);
             tbody.appendChild(tr);                 // Criada a linha, adicionamos
@@ -39,9 +39,9 @@ async function carregarDados() {
 
 function abrirpost(){
 
-    document.getElementById('titulo-post').innerText = post.titulo-post || "Sem Título";
-    document.getElementById('descricao').innerText = post.descricao-post || "Sem Descrição";
-    document.getElementById('tamanho').innerText = post.resolucao-post || "Sem Resolução";
+    document.getElementById('titulo').innerText = post.titulo || "Sem Título";
+    document.getElementById('descricao').innerText = post.descricao || "Sem Descrição";
+    document.getElementById('tamanho').innerText = post.resolucao || "Sem Resolução";
     document.getElementById('contato').innerText = post.comuniOutros || "Sem Informação";
 
     modalpost.showModal();
@@ -49,26 +49,26 @@ function abrirpost(){
 };
 
 async function gravar() {
-    let id = document.getElementById('id');
-    let titulo-post = document.getElementById('titulo-post');
-    let descricao-post = document.getElementById('descricao-post');
-    let resolucao-post = document.getElementById('resolucao-post');
+    let idPos = document.getElementById('idPos');
+    let titulo = document.getElementById('titulo');
+    let descricao = document.getElementById('descricao');
+    let resolucao = document.getElementById('resolucao');
     let comuniOutros = document.getElementById('comuniOutros');
     let i_id = 0;
-    if (isNaN(i_id = parseInt(id.value))) {         // O id precisa ser um número inteiro
+    if (isNaN(i_id = parseInt(idPos.value))) {         // O id precisa ser um número inteiro
         alert("O id precisa ser um inteiro.");
         return;
     }
     var data = JSON.stringify({                     // Criamos nosso objeto JSON Para o envio
-        id: i_id,
-        titulo-post: titulo-post.value,
-        descricao-post: descricao-post.value,
-        resolucao-post: resolucao-post.value,
+        idPos: i_id,
+        titulo: titulo.value,
+        descricao: descricao.value,
+        resolucao: resolucao.value,
         comuniOutros: comuniOutros.value,
     });
     // Se o campo ID estiver desabilitado, é porque estávamos alterando,
     // então usa PUT, senão POST, pois é um novo registro.
-    let method = id.disabled ? 'PUT' : 'POST';
+    let method = idPos.disabled ? 'PUT' : 'POST';
     try {
         const response = await fetch(url, {
             method: method,
